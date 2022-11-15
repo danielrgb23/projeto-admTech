e2a2c69daac868a44ac6092364422ab08170793e
Daniel <danielrgb2@gmail.com>
2022-11-15 15:23:41 -0300

    Feat:header finish

--- projeto_front/src/pages/Dashboard/index.jsx
+++ projeto_front/src/pages/Dashboard/index.jsx
@@ -9,7 +9,19 @@ export function Dashboard() {
       <Header />
       <div className={styles.content}>
         <Sidebar />
+
+        <div className={styles.text}>
+          <h1>Gerencie seus <br/> revendedores/afiliados com <br/> nossa ferramenta!</h1>
+        </div>
+
       </div>
+        <div className={styles.more}>
+          <h1>Quem nós somos?</h1>
+          <p>Para trazer conveniência e simplicidade de 
+            lidar com as contas e com os revendedores de 
+            pequenas franquias, atendemos com um
+            sistema de gestão de pessoas e finanças</p>
+        </div>
     </div>
   )
 }
